0611e7550984eceb32d7c2b16ccc6f83f6ac33ba
sotheapov <sotheait99@gmail.com>
2015-10-06 15:26:53 +0700

    add sever

--- public_html/server.js
+++ public_html/server.js
@@ -1,7 +1,8 @@
 var express = require('express'),
     app     = express(),
     server  = require('http').createServer(app),
-    io      = require('socket.io').listen(server);
+    io      = require('socket.io').listen(server),
+    users   = {};
 
 server.listen(3000);
 
@@ -15,14 +16,34 @@ io.sockets.on('connection', function(socket){
     //get data from server by name 
     
     //get user join
-    socket.on('user join',function(data){
-       io.sockets.emit('new user', socket.user = data); 
+    socket.on('user join',function(data, callback){ 
+        //if user exist
+        if(data in users){
+            callback(false);
+        }else
+        {
+             callback(true);
+            socket.user = data;
+            io.sockets.emit('new user', socket.user); 
+            users[socket.user] = socket;
+            allUsers();
+        }
     });
     
     socket.on('send message', function(smg)
     {
         io.sockets.emit('new message' ,{smg:smg, user:socket.user});
     });
+    function allUsers()
+    {
+        io.sockets.emit('all user', Object.keys(users));
+    }
     
+    // User diconnect
+    socket.on('disconnect', function(data){
+        if(!socket.user)return;
+        delete users[socket.user];
+        allUsers();
+    });
     
 });
