0a7d179827d76080833df32970895a5daca0d39d
sotheapov <sotheait99@gmail.com>
2015-10-06 13:54:06 +0700

    add sever

--- public_html/server.js
+++ public_html/server.js
@@ -9,3 +9,20 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
   console.log('Now server is running');
 });
+
+//connect to server
+io.sockets.on('connection', function(socket){
+    //get data from server by name 
+    
+    //get user join
+    socket.on('user join',function(data){
+       io.sockets.emit('new user', socket.user = data); 
+    });
+    
+    socket.on('send message', function(smg)
+    {
+        io.sockets.emit('new message' ,{smg:smg, user:socket.user});
+    });
+    
+    
+});
