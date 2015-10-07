f7b7a24e3b577934cbfa9c7a7f7bde0267343c74
sotheapov <sotheait99@gmail.com>
2015-10-08 04:15:52 +0700

    comprete js

--- public_html/server.js
+++ public_html/server.js
@@ -2,9 +2,27 @@ var express = require('express'),
     app     = express(),
     server  = require('http').createServer(app),
     io      = require('socket.io').listen(server),
+    mongoose= require('mongoose'),
     users   = {};
 
 server.listen(3000);
+
+//connectio to mongodb
+var mongoURI  =   "mongodb://localhost:27017/chatApp";
+var mongodb     =   mongoose.connect(mongoURI).connection;
+mongodb.on('error', function(err){
+   console.log('Erorr mongodb : '+ err.message); 
+});
+mongodb.on('open', function(){
+   console.log('Now connection to mongodb'); 
+});
+//create schema
+var chatAppSchema = mongoose.Schema({
+   user:String,
+   text:String,
+   created:{type:Date, default:Date.now}
+});
+var chatMessage = mongoose.model('message', chatAppSchema);
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
@@ -20,8 +38,7 @@ io.sockets.on('connection', function(socket){
         //if user exist
         if(data in users){
             callback(false);
-        }else
-        {
+        }else{
              callback(true);
             socket.user = data;
             io.sockets.emit('new user', socket.user); 
@@ -32,8 +49,25 @@ io.sockets.on('connection', function(socket){
     
     socket.on('send message', function(smg)
     {
-        io.sockets.emit('new message' ,{smg:smg, user:socket.user});
+        var newSmg = new chatMessage({user:socket.user, text:smg});
+        newSmg.save(function(err){
+            if(err) throw err;            
+           io.sockets.emit('new message' ,{text:smg, user:socket.user});
+        });
+        
     });
+    
+    var query = chatMessage.find({});
+    query.sort('-created').limit().exec(function(err,docs){
+        if(err){
+            console.log('error when load form database : '+ err.message);           
+        }
+        else{
+            socket.emit('old message', docs); 
+        }
+    });
+    
+    
     function allUsers()
     {
         io.sockets.emit('all user', Object.keys(users));
@@ -44,6 +78,7 @@ io.sockets.on('connection', function(socket){
         if(!socket.user)return;
         delete users[socket.user];
         allUsers();
+        io.sockets.emit('user disconnet', socket.user);
     });
     
 });
